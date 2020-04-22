769128e46eb71257b15e043d5b7eb11b7c97a046
Santiago Vasquez <svasquez@pivotal.io>
2020-04-21 21:25:46 -0400

    create project using spring initializer

---
+++ src/test/java/io/pivotal/gcloudmysqlpoc/GcloudMysqlPocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pivotal.gcloudmysqlpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcloudMysqlPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
